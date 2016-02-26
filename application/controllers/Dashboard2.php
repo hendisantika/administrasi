01a508b964aa89a324ef30b9804a9974d1bf814f
hendisantika <hendisantika@yahoo.co.id>
2016-02-26 07:48:40 +0700

    Administrasi Anggota

---
+++ application/controllers/Dashboard2.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Dashboard2 extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('dashboard2');
+	}
+}
