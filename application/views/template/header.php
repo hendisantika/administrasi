768fae7fbba288212f40baf5fc369d1790017e2c
hendisantika <hendisantika@yahoo.co.id>
2016-03-18 06:44:17 +0700

    Edit Cache

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -1,3 +1,9 @@
+<?php 
+     header("Cache-Control: private, max-age=10800, pre-check=10800");
+     header("Pragma: private");
+     header("Expires: " . date(DATE_RFC822,strtotime("+2 day")));
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
