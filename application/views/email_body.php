f78cfb0776a74f050b28f829582f6c79ef1b1e36
hendisantika <hendisantika@yahoo.co.id>
2016-03-23 18:22:49 +0700

    Email Body Forgot Password Page

---
+++ application/views/email_body.php
@@ -0,0 +1,68 @@
+<body style="background: transparent url(<MY IMAGE>;);">
+    <table align="center" border="0" cellpadding="0" cellspacing="0" width=700">
+        <tbody style="text-wrap:normal;word-wrap:break-word">
+            <tr>
+                <td>
+                    <font face="Verdana"><img height="20" src="<?php echo base_url('assets/foto/others/banner_ana1.png'); ?>" style="width: 750px; height: 20px;" width="1" /></font></td>
+            </tr>
+            <tr>
+                <td>
+                    <font face="Verdana"><img height="80" src="<?php echo base_url('assets/foto/others/banner_ana1.png'); ?>" style="width: 750px; height: 80px;" width="750" /></font></td>
+            </tr>
+            <tr>
+                <td align="left" bgcolor="#FFFFFF" style="padding: 10px;">
+                    <p>
+                        <span style="font-size: 14pt;"><strong><span style="color: rgb(0, 0, 128);"><font face="Verdana">Password Recovery</font></span></strong></span><br />
+                        <br />
+                        <span style="font-size: 10pt;"><span style="font-family: arial,helvetica,sans-serif;">Bismillahirrohmanirrohim.
+                                <p>
+                                    Ikhwatu Iman, Antum tadi meminta untuk dikirimkan password via alamat email ini (<?php echo $forgot['email'] ?>)
+                                    <br>
+                                    Berikut adalah password antum :  <br>
+                                    NPA             : <?php echo $forgot['npa'] ?> <br>
+                                    Nama Lengkap    : <?php echo $forgot['nama'] ?> <br>
+                                    Password        : <?php echo $forgot['password'] ?> <br>
+                                    Email           : <?php echo $forgot['email'] ?> <br>
+                                    <?php $url = base_url(); ?>
+                                    Silahkan login kembali menggunakan password tersebut ke url <a href="<?php echo $url ?>"><?php echo $url ?></a>. <br>
+                                </p>
+                                <br>
+                                <p>
+                                    =============================<br>
+                                    Syukron Katsiron <br>
+                                    Best Regards.<br>
+                                    =============================<br>
+                                    Tetap Optimis, <br>
+                                    kerja optimal. <br>
+                                    Hendi Santika<br><br>
+                                   
+                                    KOMINFO PP. PEMUDA PERSIS<br><br>
+
+                                    Professional<br>
+                                    Respect to others<br>
+                                    Innovation<br>
+                                    Determination<br>
+                                    Enterpreneurship<br>
+                                    =============================<br>
+                                </p>
+                                <br />
+                                <br />
+                            </span></span></p>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <font face="Verdana"><img height="90" src="<MY IMAGE>" style="width: 750px; height: 90px;" width="750" /></font></td>
+            </tr>
+            <tr>
+                <td align="left" bgcolor="#83B5D4" style="font-size: 9px; color: rgb(255, 255, 255);">
+                    <font face="Verdana"><img height="10" src="<MY IMAGE>" style="width: 10px; height: 10px;" width="10" /></font></td>
+            </tr>
+            <tr>
+                <td align="center" bgcolor="#39ac39" style="padding: 10px;" valign="middle">
+                    <font color="#FFFFFF" face="Verdana" style="font-size: 10px; line-height: 1.5em;">PP. Pemuda Persatuan Islam (PERSIS) | Jl. Perintis Kemerdekaan No. 2 Bandung - Jawa Barat | Tel: +62 8111 847 111 | Tel: +62 813 21 411 881 | <a href="mailto:kominfo@pemuda.persis.or.id" style="color: rgb(255, 255, 255); text-decoration: none;">kominfo@pemuda.persis.or.id</a> | <a href="http://pemuda.persis.or.id/administrasi/" style="color: rgb(255, 255, 255); text-decoration: none;" target="_blank">pemuda.persis.or.id/administrasi</a><br />
+                    </font></td>
+            </tr>
+        </tbody>
+    </table>
+</body>
