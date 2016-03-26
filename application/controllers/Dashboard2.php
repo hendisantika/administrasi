29453341db5813a64823ec50fd96978b8652983d
hendisantika <hendisantika@yahoo.co.id>
2016-03-27 06:55:22 +0700

    Edit Dashboard2 Controller

--- application/controllers/Dashboard2.php
+++ application/controllers/Dashboard2.php
@@ -1,10 +1,14 @@
 <?php
+
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Dashboard2 extends CI_Controller {
 
-	public function index()
-	{
-		$this->load->view('dashboard2');
-	}
+    public function index() {
+        if ($this->session->status_login != 'logged_in') {
+            redirect('auth');
+        }
+        $this->load->view('dashboard2');
+    }
+
 }
