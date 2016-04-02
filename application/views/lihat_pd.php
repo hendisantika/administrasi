eb5536c62578f8ba2195b222cc0f252e79ef6886
hendisantika <hendisantika@yahoo.co.id>
2016-04-02 16:01:35 +0700

    Edit Lihat PD

--- application/views/lihat_pd.php
+++ application/views/lihat_pd.php
@@ -35,6 +35,7 @@
                             <table id="data_pd" class="table table-bordered table-striped">
                                 <thead>
                                     <tr>
+                                        <th>NO</th>
                                         <th>Foto</th>
                                         <th>Kode PD</th>
                                         <th>Kode PW</th>
@@ -46,9 +47,10 @@
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <?php foreach ($a as $data_pd) { ?>
+                                    <?php $no =1; foreach ($a as $data_pd) { ?>
                                     <tr class="pilih">
-                                            <td><img src="<?php echo base_url('/assets/foto/pc/' . $data_pd->foto); ?>" class="img-responsive img-rounded center-block" width="150"></td>
+                                            <td><?php echo $no; ?></td>
+                                            <td><img src="<?php echo base_url('/assets/foto/pd/' . $data_pd->foto); ?>" class="img-responsive img-rounded center-block" width="150"></td>
                                             <td><?php echo $data_pd->kd_pd; ?></td>
                                             <td><?php echo $data_pd->kd_pw; ?></td>
                                             <td><?php echo $data_pd->pd; ?></td>
@@ -71,10 +73,11 @@
                                                 </a>
                                             </td>
                                         </tr>
-                                    <?php } ?>
+                                    <?php $no++; } ?>
                                 </tbody>
                                 <tfoot>
                                     <tr>
+                                        <th>NO</th>
                                         <th>Foto</th>
                                         <th>Kode PD</th>
                                         <th>Kode PW</th>
@@ -112,7 +115,7 @@
     $('#data_pd tbody').on('click', 'tr', function () {
         var data = table.row( this ).data();
 //        alert( 'Antum milih data : \n'+data[1]+'|'+data[2]+'|'+data[3]+'|'+data[4]+'|'+data[5]+'|'+data[6]+'|'+data[7]+'|'+data[8]+'|'+data[9]+'|'+data[10]);
-        window.location = "<?php echo site_url() ?>profil/pd_details/"+data[1]; 
+        window.location = "<?php echo site_url() ?>profil/pd_details/"+data[2]; 
     } );
 } );
 </script>
