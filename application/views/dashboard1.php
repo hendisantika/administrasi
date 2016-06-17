bcf789840a75ab9663c7498678fde219198ca09e
hendisantika <hendisantika@yahoo.co.id>
2016-06-17 13:34:15 +0700

    Tambah link user terdaftar

--- application/views/dashboard1.php
+++ application/views/dashboard1.php
@@ -147,7 +147,7 @@
                     <div class="icon">
                         <i class="ion ion-person-add"></i>
                     </div>
-                    <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                    <a href="<?php echo base_url('user/lihat_user')?>" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
                 </div>
             </div><!-- ./col -->
         </div><!-- /.row -->
